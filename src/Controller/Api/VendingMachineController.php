9a3e92b91dc277d67f7f86bb4a910508e86ad584
alguive <a.guijarro.vera@gmail.com>
2026-01-29 09:44:24 +0100

    Create base logic to get machine status

--- src/Controller/Api/VendingMachineController.php
+++ src/Controller/Api/VendingMachineController.php
@@ -21,9 +21,11 @@ class VendingMachineController
      * @throws \Exception
      */
     #[Route('/api/vending-machine', name: 'api_vending_machine', methods: ['GET'])]
-    public function index(Request $request): JsonResponse
+    public function index(): JsonResponse
     {
-        return new JsonResponse([]);
+        $machineStatus = $this->vendingMachineRepository->read();
+
+        return JsonResponse::fromJsonString($machineStatus->toJson());
     }
 
     /**
---
+++ src/DTO/VendingMachineStatus.php
@@ -0,0 +1,130 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DTO;
+
+class VendingMachineStatus
+{
+    public function __construct(
+        protected float $balance,
+        protected array $coins,
+        protected array $items,
+    ) {
+    }
+
+    /**
+     * Create DTO from JSON.
+     *
+     * @param string $json
+     * @return VendingMachineStatus
+     */
+    public static function fromJson(string $json): VendingMachineStatus
+    {
+        $data = self::parseJson($json);
+
+        return new VendingMachineStatus(
+            balance: $data['balance'],
+            coins: $data['coins'],
+            items: $data['items'],
+        );
+    }
+
+    /**
+     * Get current balance.
+     *
+     * @return float
+     */
+    public function getBalance(): float
+    {
+        return $this->balance;
+    }
+
+    /**
+     * Get coins available.
+     *
+     * @return array
+     */
+    public function getCoins(): array
+    {
+        return $this->coins;
+    }
+
+    /**
+     * Get items available.
+     *
+     * @return array
+     */
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    /**
+     * Update balance with the inserted coins.
+     *
+     * @param float $balance
+     * @return void
+     */
+    public function setBalance(float $balance): void
+    {
+        $this->balance = $balance;
+    }
+
+    /**
+     * Update Coins.
+     *
+     * @param array $coins
+     * @return void
+     */
+    public function setCoins(array $coins): void
+    {
+        $this->coins = $coins;
+    }
+
+    /**
+     * Update Items.
+     *
+     * @param array $items
+     * @return void
+     */
+    public function setItems(array $items): void
+    {
+        $this->items = $items;
+    }
+
+    /**
+     * Transform DTO into an array.
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+            'balance' => $this->balance,
+            'coins' => $this->coins,
+            'items' => $this->items,
+        ];
+    }
+
+    /**
+     * Transform DTO into a JSON string.
+     *
+     * @return string
+     */
+    public function toJson(): string
+    {
+        return \json_encode($this->toArray());
+    }
+
+    /**
+     * Decode JSON into an array.
+     *
+     * @param string $json
+     * @return array
+     */
+    protected static function parseJson(string $json): array
+    {
+        return \json_decode($json, true);
+    }
+}
